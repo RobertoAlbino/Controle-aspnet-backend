82bb110dbe3fa9343553626eeda38da2c131af66
Roberto Albino <junior_wash@hotmail.com>
2018-03-28 17:58:23 -0300

    Implementing the context of the application

---
+++ src/Controle.Infra.Data/Context/ControleContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Controle.Domain.Models;
+
+namespace Controle.Infra.Data.Context
+{
+    public class ControleContext : DbContext
+    {
+        public DbSet<Cidade> Cidades { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Equipamento> Equipamentos { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<OrdemServico> OrdemServico { get; set; }
+        public DbSet<Pais> Pais { get; set; }
+        public DbSet<TipoEquipamento> TipoEquipamento { get; set; }
+    }
+}
