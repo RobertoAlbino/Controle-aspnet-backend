477749b08a4598e12754c6e2fd30f023d1ed4cea
Roberto Albino <junior_wash@hotmail.com>
2018-03-28 17:42:03 -0300

    Implementando a camada de dados

---
+++ src/Controle.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Controle.Domain.Core.Models;
+
+namespace Controle.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity<TEntity>
+    {
+        TEntity GetById(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        int SaveChanges();
+    }
+}
---
+++ src/Controle.Domain/Models/Interfaces/IOrdemServicoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Controle.Domain.Interfaces;
+
+namespace Controle.Domain.Models.Interfaces
+{
+    public interface IOrdemServicoRepository : IRepository<OrdemServico>
+    {
+
+    }
+}
