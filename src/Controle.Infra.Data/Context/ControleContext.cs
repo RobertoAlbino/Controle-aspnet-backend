4428869e64f5771173382a3745999668e8756550
Roberto Albino <junior_wash@hotmail.com>
2018-03-29 09:13:20 -0300

    Create the first Migration and Mapping tests

--- src/Controle.Infra.Data/Context/ControleContext.cs
+++ src/Controle.Infra.Data/Context/ControleContext.cs
@@ -1,17 +1,106 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Controle.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Controle.Infra.Data.Context
 {
     public class ControleContext : DbContext
     {
+        #region Entidades
         public DbSet<Cidade> Cidades { get; set; }
-        public DbSet<Cliente> Clientes { get; set; }
-        public DbSet<Endereco> Enderecos { get; set; }
-        public DbSet<Equipamento> Equipamentos { get; set; }
+       // public DbSet<Cliente> Clientes { get; set; }
+       // public DbSet<Endereco> Enderecos { get; set; }
+        //public DbSet<Equipamento> Equipamentos { get; set; }
         public DbSet<Estado> Estados { get; set; }
-        public DbSet<OrdemServico> OrdemServico { get; set; }
+        //public DbSet<OrdemServico> OrdemServico { get; set; }
         public DbSet<Pais> Pais { get; set; }
-        public DbSet<TipoEquipamento> TipoEquipamento { get; set; }
+        //public DbSet<TipoEquipamento> TipoEquipamento { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Cidade
+            modelBuilder.Entity<Cidade>()
+                .ToTable("Cidades")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Cidade>()
+                .Property(x => x.Nome)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+
+            modelBuilder.Entity<Cidade>()
+               .Ignore(x => x.ValidationResult);
+            #endregion
+
+            #region Cliente
+            modelBuilder.Entity<Cliente>()
+                .ToTable("Clientes")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Cliente>()
+                .Property(x => x.Nome)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            modelBuilder.Entity<Cliente>()
+                .Property(x => x.Email)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            modelBuilder.Entity<Cliente>()
+                .Ignore(x => x.Endereco);
+
+            modelBuilder.Entity<Cliente>()
+                .Property(x => x.Telefone)
+                .HasColumnType("varchar(15)")
+                .IsRequired();
+
+            modelBuilder.Entity<Cliente>()
+              .Ignore(x => x.ValidationResult);
+            #endregion
+
+            #region Estado
+            modelBuilder.Entity<Estado>()
+                .ToTable("Estados")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Estado>()
+                .Property(x => x.Nome)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+
+            modelBuilder.Entity<Estado>()
+              .Ignore(x => x.ValidationResult);
+            #endregion
+
+            #region Pais
+            modelBuilder.Entity<Pais>()
+                .ToTable("Paises")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Pais>()
+                .Property(x => x.Nome)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            modelBuilder.Entity<Pais>()
+              .Ignore(x => x.ValidationResult);
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("Connection"));
+        }
+
     }
 }
---
+++ src/Controle.Infra.Data/Migrations/20180329120556_Initial.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Controle.Infra.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CascadeMode = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(150)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cidades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CascadeMode = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CascadeMode = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(150)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CascadeMode = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paises", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cidades");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Estados");
+
+            migrationBuilder.DropTable(
+                name: "Paises");
+        }
+    }
+}
