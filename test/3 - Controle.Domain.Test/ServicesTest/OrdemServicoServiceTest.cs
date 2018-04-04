e98fde1ae0e4735ed3dfbecec942e48aa053a1c0
Roberto Albino Junior <roberto.junior@sistemainfo.corp>
2018-04-04 18:00:46 -0300

    Adicionando um caso de teste

--- test/3 - Controle.Domain.Test/ServicesTest/OrdemServicoServiceTest.cs
+++ test/3 - Controle.Domain.Test/ServicesTest/OrdemServicoServiceTest.cs
@@ -1,13 +1,22 @@
-﻿using Xunit;
+﻿using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Xunit.Abstractions;
+using NSubstitute;
+using Controle.Services.Api.Controllers;
+
 
 namespace Controle.Test.Controle.Domain.Test.ServicesTest
 {
-    public class OrdemServicoServiceTest
+    public class OrdemServicoServiceTest : Controller
     {
+        private readonly ITestOutputHelper output;
+
         [Fact]
-        public void Testar()
+        public void VerificarSeIntegracaoEstaFuncionando()
         {
-            Assert.True(2 == 1);
+            var ordemServicoIntegrar = new OrdemServicoController();
+            output.WriteLine(ordemServicoIntegrar.Integrar().ToString());
+            Assert.True(ordemServicoIntegrar.Integrar() == Json("Ordem de serviço integrada com sucesso."));
         }
     }
 }
