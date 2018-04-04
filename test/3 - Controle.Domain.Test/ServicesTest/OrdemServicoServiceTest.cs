4a50c842b7ea8d6ed69e4afd1e991a94d9665bb4
Roberto Albino Junior <roberto.junior@sistemainfo.corp>
2018-04-04 17:06:06 -0300

    Adicionando o projeto de testes na solução

---
+++ test/3 - Controle.Domain.Test/ServicesTest/OrdemServicoServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Controle.Test.Controle.Domain.Test.ServicesTest
+{
+    public class OrdemServicoServiceTest
+    {
+        [Fact]
+        public void Testar()
+        {
+            Assert.True(2 == 1);
+        }
+    }
+}
